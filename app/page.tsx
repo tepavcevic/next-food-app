9cec786e0b038cca623740e2ba866f166a637915
tepavcevic <djordje.tepa@gmail.com>
2023-12-16 23:23:52 +0100

    Move editorconfig to the root

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
+import Link from 'next/link';
+
 export default function Home() {
   return (
     <main>
       <img src="/logo.png" alt="A server surrounded by magic sparkles." />
       <h1>Neeext pleaseeee</h1>
       <p>🔥 Let&apos;s get started! 🔥</p>
+      <p>
+        <Link href="/about">About Us</Link>
+      </p>
     </main>
   );
 }
